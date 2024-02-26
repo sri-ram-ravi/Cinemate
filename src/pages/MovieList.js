31229f051096d6c588932ab277be047391e4941a
sri-ram-ravi <sriramravigit@gmail.com>
2024-02-26 14:10:00 +0530

    Update MovieList.js

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,22 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card } from '../componenets/index'
 
 const MovieList = () => {
+  const [movie,setMovie]=useState([]);
+  useEffect(()=>{
+    const movieResponse=async()=>{
+      const response=await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=aead2373813b29cd09bf2e776f9de2ee");
+      const responseJson=await response.json();
+      setMovie(responseJson.result)
+    }
+    movieResponse()
+  },[])
   return (
     <div className='flex justify-between flex-wrap gap-3'>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        {
+          movie.map((movie)=>{
+            <Card/>
+          })
+        }
     </div>
   )
 }
